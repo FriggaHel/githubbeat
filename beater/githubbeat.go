1e4b22a1b4a09698759ad74e855e573e370d72a8
Felix P <felix@wingz.me>
2017-06-26 12:22:10 +0200

    Add ability to fecth past

--- beater/githubbeat.go
+++ beater/githubbeat.go
@@ -10,6 +10,7 @@ import (
 	"github.com/elastic/beats/libbeat/publisher"
 
 	"github.com/FriggaHel/githubbeat/config"
+	"github.com/google/go-github/github"
 )
 
 type Githubbeat struct {
@@ -40,6 +41,10 @@ func (bt *Githubbeat) Run(b *beat.Beat) error {
 
 	if bt.config.GithubToken != nil {
 		logp.Info("Using Github Token")
+	}
+
+	if bt.config.FetchPast == true {
+		bt.FetchPast()
 	}
 
 	for {
@@ -91,4 +96,111 @@ func (bt *Githubbeat) Run(b *beat.Beat) error {
 func (bt *Githubbeat) Stop() {
 	bt.client.Close()
 	close(bt.done)
+}
+
+func (bt *Githubbeat) FetchPast() {
+	for _, t := range bt.config.Repositories {
+		r := NewRepository(bt, t.Account, t.Name)
+
+		// Fetch Forks + Issues + Stars
+		forks, _ := r.GetForks()
+		issues, _ := r.GetIssues("all")
+		stargazers, _ := r.GetStargazers()
+
+		// Beginning + Short rewind
+		cur := bt.FindFirstDate(stargazers, issues, forks).Add(bt.config.Step * -1)
+
+		// Indexes
+		var stargazersIdx int = 0
+		var forksIdx int = 0
+		var issuesIdx int = 0
+
+		now := time.Now()
+		for {
+			stargazersCount := bt.CountStargazers(cur, stargazers, &stargazersIdx)
+			issuesCount, prCount := bt.CountIssues(cur, issues, &issuesIdx)
+			forksCount := bt.CountForks(cur, forks, &forksIdx)
+
+			event := common.MapStr{
+				"@timestamp": common.Time(cur),
+				"type":       "githubbeat",
+				"repository": common.MapStr{
+					"fullname":          fmt.Sprintf("%s/%s", t.Account, t.Name),
+					"account":           t.Account,
+					"name":              t.Name,
+					"forks_count":       forksCount,
+					"network_count":     0,
+					"open_issues_count": issuesCount,
+					"open_pr_count":     prCount,
+					"stargazers_count":  stargazersCount,
+					"subscribers_count": 0,
+					"watchers_count":    0,
+				},
+			}
+			bt.client.PublishEvent(event)
+			if cur.After(now) {
+				break
+			}
+			cur = cur.Add(bt.config.Step)
+		}
+	}
+}
+
+func (bt *Githubbeat) CountForks(t time.Time, forks []*github.Repository, index *int) int {
+	for *index < len(forks) && forks[*index].CreatedAt.Time.Before(t) {
+		(*index)++
+	}
+	return *index
+}
+
+func (bt *Githubbeat) CountIssues(t time.Time, issues []*github.Issue, index *int) (int, int) {
+	issueCnt := 0
+	prCnt := 0
+
+	for *index < len(issues) && issues[*index].CreatedAt.Before(t) {
+		(*index)++
+	}
+
+	// Keep It safe
+	rIndex := *index
+	if rIndex == len(issues) {
+		rIndex--
+	}
+	for ; rIndex >= 0; rIndex-- {
+		if issues[rIndex].ClosedAt == nil || issues[rIndex].ClosedAt.After(t) {
+			if issues[rIndex].PullRequestLinks == nil {
+				issueCnt++
+			} else {
+				prCnt++
+			}
+		}
+	}
+	return issueCnt, prCnt
+}
+
+func (bt *Githubbeat) CountStargazers(t time.Time, stargazers []*github.Stargazer, index *int) int {
+	for *index < len(stargazers) && stargazers[*index].StarredAt.Time.Before(t) {
+		(*index)++
+	}
+	return *index
+}
+
+func (bt *Githubbeat) FindFirstDate(stargazers []*github.Stargazer, issues []*github.Issue, forks []*github.Repository) time.Time {
+	t := time.Now()
+
+	// Assume this is sorted (api seems to)
+	if len(stargazers) > 0 && stargazers[0].StarredAt.Time.Before(t) {
+		t = stargazers[0].StarredAt.Time
+	}
+
+	// Assume they are sorted => https://developer.github.com/v3/issues/#list-issues-for-a-repository
+	if len(issues) > 0 && issues[0].CreatedAt.Before(t) {
+		t = *issues[0].CreatedAt
+	}
+
+	// Assume this is sorted => https://developer.github.com/v3/repos/forks/#list-forks
+	if len(forks) > 0 && forks[0].CreatedAt.Before(t) {
+		t = forks[0].CreatedAt.Time
+	}
+	return t
 }
--- config/config.go
+++ config/config.go
@@ -7,7 +7,9 @@ import "time"
 
 type Config struct {
 	Period       time.Duration `config:"period"`
+	Step         time.Duration `config:"step"`
 	GithubToken  *string       `config:"github_token"`
+	FetchPast    bool          `config:"fetch_past"`
 	Repositories []Repository  `config:"repositories"`
 }
 
@@ -18,6 +20,8 @@ type Repository struct {
 
 var DefaultConfig = Config{
 	Period:       1 * time.Second,
+	Step:         1 * time.Hour,
 	GithubToken:  nil,
+	FetchPast:    false,
 	Repositories: []Repository{},
 }
